4f257bbf1a17b33bbd59b4e9c683db352f1d6fa6
Alex-361 <alexvxxx24@gmail>
2024-11-10 19:55:42 -0300

    bbb

--- src/starship.test.js
+++ src/starship.test.js
@@ -1,12 +1,9 @@
 const axios = require('axios');
-const BASE_URL = 'https://swapi.dev/api/starships/';
 
-describe('Star Wars Starship API', () => {
-  it('should fetch the details of the first Star Wars starship', async () => {
-    const response = await axios.get(BASE_URL + '2/'); // ID 2 corresponde à nave "CR90 corvette"
-    
+describe("Star Wars Starship API", () => {
+  it("should fetch the details of the first Star Wars starship ", async () => {
+    const response = await axios.get("https://swapi.dev/api/starships/");
     expect(response.status).toBe(200);
-    expect(response.data.name).toBe('CR90 corvette');
-    expect(response.data.model).toBe('CR90 corvette');
+    expect(response.data).toHaveProperty("nome/model", "CR90 corvette");
   });
 });
