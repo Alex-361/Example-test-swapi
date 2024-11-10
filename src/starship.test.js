a3bfc319295c5cd85afa5f3105a8a823acb6c988
Alex-361 <alexvxxx24@gmail>
2024-11-10 20:02:20 -0300

    novo

--- src/starship.test.js
+++ src/starship.test.js
@@ -1,12 +1,18 @@
+// src/tests/starship.test.js
 const axios = require('axios');
-const BASE_URL = 'https://swapi.dev/api/starships/';
 
-describe('Star Wars Starship API', () => {
-  it('should fetch the details of the first Star Wars starship', async () => {
-    const response = await axios.get(BASE_URL + '2/'); // ID 2 corresponde à nave "CR90 corvette"
-    
+describe("Star Wars Starship API", () => {
+  it("deve buscar os detalhes da nave Millennium Falcon", async () => {
+    const response = await axios.get("https://swapi.dev/api/starships/10/"); // ID 10 é da Millennium Falcon
     expect(response.status).toBe(200);
-    expect(response.data.name).toBe('CR90 corvette');
-    expect(response.data.model).toBe('CR90 corvette');
+    expect(response.data).toHaveProperty("name", "Millennium Falcon");
+  });
+
+  it("deve retornar erro ao buscar uma nave inexistente", async () => {
+    try {
+      await axios.get("https://swapi.dev/api/starships/99999/");
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
   });
 });
